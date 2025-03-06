b947644319d5e2146c7906aa2adc024a80f728b2
Beadko <beadko@gmail.com>
2025-03-06 09:10:58 +0700

    Complete the task

--- internal/handler.go
+++ internal/handler.go
@@ -2,7 +2,14 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v3"
 )
+
+type pathURL struct {
+	path string
+	URL  string
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -11,8 +18,15 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if redirectURL, exists := pathsToUrls[path]; exists {
+			http.Redirect(w, r, redirectURL, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -32,6 +46,15 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var pathURLs []pathURL
+	err := yaml.Unmarshal(yml, &pathURLs)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := make(map[string]string)
+	for _, p := range pathURLs {
+		pathsToUrls[p.path] = p.URL
+	}
+
+	return MapHandler(pathsToUrls, fallback), nil
 }
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	urlshort "github.com/Beadko/gophecizes_urlShortener/internal"
 )
+
+var (
+	file = flag.String("file", "paths.yaml", "Path to url file")
+)
+
+func readFile(string) ([]byte, error) {
+	f, err := os.ReadFile(*file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open the file: %w", err)
+	}
+	return f, nil
+}
 
 func main() {
 	mux := defaultMux()
@@ -17,14 +31,12 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+	yaml, err := readFile(*file)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
